35a907e4a97e83db531bf65b631a35445c038f62
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2022-06-15 15:19:15 -0300

    improves images handling

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,25 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600, 900, 1200, 1500, 1800],
+    formats: ["avif", "webp", "jpeg", null],
+    outputDir: "./client/assets/image/opmized/",
+    urlPath: "/image/opmized/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline",
+  });
+}
+
 module.exports = function (eleventyConfig) {
   // Route overides
   eleventyConfig.addPassthroughCopy({ "client/assets": "/" }, () => {
@@ -5,4 +27,8 @@ module.exports = function (eleventyConfig) {
       passthroughFileCopy: true,
     };
   });
+
+  // Image optmizer
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 };
